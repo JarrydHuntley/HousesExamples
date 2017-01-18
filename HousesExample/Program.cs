5ef6ea0c800248935799108939579ced9cc5ea75
Your Name <you@example.com>
2017-01-18 14:01:46 -0500

    Added a class student

--- HousesExample/Program.cs
+++ HousesExample/Program.cs
@@ -11,21 +11,7 @@ namespace HousesExample
     {
         static void Main(string[] args)
         {
-            House house1 = new House("Green", 4, 1000, 3);
-            House house2 = new House("Blue", 700);
-            House house3 = new House();
-
-            //house1.DisplayHouseInfo();
-            //house2.DisplayHouseInfo();
-            //house3.DisplayHouseInfo();
-
-            //house2.Paint("Yellow");
-
-            //house2.DisplayHouseInfo();
-            house2.Name = "The blue house!";
-
-            Console.WriteLine("This is the name of the house: "+house2.Name);
-            
+            Student student1 = new Student("Bob", 25);  
         }
     }
 }
